fc4483ebbbf85fe7339d1cdb65473f56218a882f
Surya <suryapanneer04@gmail.com>
2024-06-21 11:19:11 +0530

    npm example

--- NodeJs/2.5 sample Npm/index.js
+++ NodeJs/2.5 sample Npm/index.js
@@ -1,12 +1,15 @@
-import { generate, count } from "random-words";
+// import { generate, count } from "random-words";
 
-console.log(generate(5));
-console.log(generate({ minLength: 2 }));
-console.log(
-    generate({
-      exactly: 15,
-      wordsPerString: 3,
-      formatter: (word) => word.toUpperCase(),
-    })
-  );
-  
+// console.log("unique words "+generate(5));
+// console.log(generate({ minLength: 2 }));
+// console.log(
+//     generate({
+//       exactly: 15,
+//       wordsPerString: 3,
+//       formatter: (word) => word.toUpperCase(),
+//     })
+// );
+
+import SearchBox from '@govuk-react/search-box';
+
+console.log(SearchBox);

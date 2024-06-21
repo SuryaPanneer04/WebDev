eec7e0287c9001470fb09fb414ceb3b6a8818e18
Surya <suryapanneer04@gmail.com>
2024-06-21 10:41:21 +0530

    random-word Example

---
+++ NodeJs/2.5 sample Npm/index.js
@@ -0,0 +1,11 @@
+import { generate, count } from "random-words";
+
+console.log(generate(5));
+console.log(generate({ minLength: 2 }));
+console.log(
+    generate({
+      exactly: 15,
+      wordsPerString: 3,
+      formatter: (word) => word.toUpperCase(),
+    })
+  );
